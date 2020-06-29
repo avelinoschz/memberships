8af247f2770fd1b6b89ebdb60d23bc501a85a0a6
Avelino Sanchez <avelino.schz@gmail.com>
2020-06-29 16:54:20 -0500

    feat: add run.sh file

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-export $(cat ../configs/example.env | xargs)
+# exports all the neccesary environment variables
+export $(cat configs/example.env | xargs)
 
-echo DONE!
+# prepare all dependencies needed
+deploy/docker-compose up
+
+# run the main program
+# TODO change it to run via docker
+go run ./cmd/memberships-api/main.go
